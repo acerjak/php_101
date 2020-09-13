f984cb08783402e48714a5411d1991c71d5a5838
acerjak <62491401+acerjak@users.noreply.github.com>
2020-09-12 21:36:52 -0700

    updated to work on new activity

---
+++ array.php
@@ -0,0 +1,33 @@
+<?php
+
+// $greeting = 'Hello World';
+
+$names = [
+    'Amanda',
+    'Matt',
+    'Charles'
+];
+
+$animals = [
+    'Horse',
+    'Dog',
+    'Cat',
+    'Monkey',
+    'Elephant',
+    'Koala'
+];
+
+$person = [
+    'age' => 28,
+    'hair' => 'brown',
+    'career' => 'jr. php developer'
+];
+
+$person['name'] = 'Amanda';
+
+// foreach ($names as $name) {
+
+//     echo $name . ',';
+// }
+
+require 'array.view.php';
