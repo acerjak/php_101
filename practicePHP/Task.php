c11943ebb1fb443d884f9b561d587d91f1b226f2
acerjak <62491401+acerjak@users.noreply.github.com>
2020-09-15 15:46:04 -0700

    moved Task Class to practice folder since this is being handled by the connection in bootstrap folder

---
+++ practicePHP/Task.php
@@ -0,0 +1,31 @@
+<?php
+
+class Task {
+    
+    public $description;
+
+    public $completed;
+
+    // public function __construct($description)
+    // {
+    //     $this->description = $description;
+    // }
+// //if completed value is false, change to true
+//     public function complete()
+//     {
+//         $this->completed = true;
+//     }
+// //check boolean value of completed
+//     public function isComplete()
+//     {
+//         return $this->completed;
+//     }
+}
+
+// $tasks = [
+//     new Task('Go to the store'),
+//     new Task('Feed the cat.'),
+//     new Task('Water the plants.')
+// ];
+
+// $tasks[0]->complete();
